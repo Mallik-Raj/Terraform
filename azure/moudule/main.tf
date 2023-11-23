49690b2f7f5df92e43c1784909e4dc3898194c5e
mallik-raj <53124649+mallik-raj@users.noreply.github.com>
2023-11-23 23:32:40 +0530

    Update main.tf

--- azure/moudule/main.tf
+++ azure/moudule/main.tf
@@ -6,3 +6,7 @@ module "my_app_service" {
   app_service_name    = "myAppService"
   app_service_plan_name = "myAppServicePlan"
 }
+
+output "my_app_service_url" {
+  value = module.my_app_service.app_service_url
+}

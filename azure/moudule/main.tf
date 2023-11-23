515e815346eccfdd68a1732b1aab4c3114f2a32e
mallik-raj <53124649+mallik-raj@users.noreply.github.com>
2023-11-23 23:11:42 +0530

    Update main.tf

--- azure/moudule/main.tf
+++ azure/moudule/main.tf
@@ -1,1 +1,8 @@
+module "my_app_service" {
+  source = _service_module"
 
+  resource_group_name = "myResourceGroup"
+  location            = "East US"
+  app_service_name    = "myAppService"
+  app_service_plan_name = "myAppServicePlan"
+}

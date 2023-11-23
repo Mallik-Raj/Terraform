b383f722da354752f0d660e060447eb417a1bb85
mallik-raj <53124649+mallik-raj@users.noreply.github.com>
2023-11-23 22:56:04 +0530

    Update varaible.tf

--- azure/appserivce/varaible.tf
+++ azure/appserivce/varaible.tf
@@ -1,1 +1,16 @@
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the App Service."
+}
+
+variable "location" {
+  description = "The Azure region where the resource group and App Service should be created."
+}
+
+variable "app_service_name" {
+  description = "The name of the App Service."
+}
+
+variable "app_service_plan_name" {
+  description = "The name of the App Service Plan."
+}
 
